2c5065e3da3990f5e2a009a9b16d3137233f1c30
ankitklu <ankit.klu.2022@gmail.com>
2026-09-03 23:14:51 +0530

    GenAI: Document Loaders

--- Gen_AI/RAG/Document Loaders/pdf.py
+++ Gen_AI/RAG/Document Loaders/pdf.py
@@ -1,7 +1,14 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import TokenTextSplitter
 
 data = PyPDFLoader("GRU.pdf")
 
 docs = data.load()
 
-print(docs[14].page_content)
+splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=10)
+
+chunks = splitter.split_documents(docs)
+
+for chunk in chunks:
+    print(chunk.page_content)
+    print("--------------------------------------------------")
